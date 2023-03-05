1c9bf042948aa132ba76def7f63143da806eba46
Robert Best <chessscholar@gmail.com>
2023-03-05 15:03:11 -0500

    fixed comments and logging

--- collaborator.js
+++ collaborator.js
@@ -2,6 +2,8 @@ import {Graph} from './graph.js'
 import {drop,dropl,dropu} from './drop.js'
 import {display} from './display.js'
 
+console.log("collaborator.js import worked")
+
 // const beam = await fetch('./SituatedInstitutions.jsonl')
 //   .then(req => req.text())
 //   .then(text => text.trim()
@@ -39,4 +41,4 @@ let graph = browser.runtime.getBackgroundPage().graph;
 console.log("collaborator.js was run");
 //This might work, but I don't think this script knows what graph is? use local storage for graph passing?
 let beam = [{ name: "graph", graph: graph }];
-
+*/
