b0bdb66c25b79b82c80ae06c9e83db064fdb5406
Robert Best <chessscholar@gmail.com>
2023-03-07 22:23:24 -0500

    got dropping graphs on collaborator working

--- collaborator.js
+++ collaborator.js
@@ -1,44 +1,38 @@
+console.log("collaborator.js import worked");
 import { Graph } from "./graph.js";
 import { drop, dropl } from "./drop.js";
 import { display } from "./display.js";
 import { get } from "./idb-keyval@6.2.0-dist-index.js";
-
-console.log("collaborator.js import worked");
-
-const target = document.getElementById("target");
-
 let graph = await get("graph");
 
+const target = document.getElementById("target");
 let beam = [
   {
     name: `tabs ${Date.now()}.graph.json`,
     graph: new Graph(graph.nodes, graph.rels),
   },
 ];
-
 display(beam, target);
 
-window.over = function (event) {
-  event.preventDefault();
-};
-
-window.drop = async function (event) {
-  event.preventDefault();
-  beam.push(
+const dropping = async function (event) {
+  console.log("dropped inside function now");
+  beam = [
     ...(await dropl(event, ".jsonl")),
     ...(await drop(event, ".graph.json"))
-  );
+  ];
   display(beam, target);
 };
 
 target.addEventListener("drop", (event) => {
-  drop(event);
+  event.preventDefault();
+  console.log("drop heard");
+  dropping(event);
 });
 
 target.addEventListener("dragover", (event) => {
-  over(event);
+  event.preventDefault();
 });
 
 target.addEventListener("dragenter", (event) => {
-  over(event);
+  event.preventDefault();
 });
